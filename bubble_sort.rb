990acbb6bca63e4f261db68ad9b3d450e3091213
James Grieve <pocojames@hotmail.com>
2020-07-08 14:42:44 -0700

    Bubble sort complete

---
+++ bubble_sort.rb
@@ -0,0 +1,16 @@
+def bubble_sort(array)
+    sorted = false
+
+    while !sorted
+        sorted = true
+        array.each_with_index do |num, i|
+         if i < array.length - 1 && array[i] > array[i + 1] 
+            array[i], array[i + 1] = array[i + 1], array[i]
+            sorted = false
+         end
+        end
+    end
+    array
+end
+
+p bubble_sort([4,3,78,2,0,2])
